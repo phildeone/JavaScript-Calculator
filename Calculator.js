0d50bafd6cd25ebbc931a9e4d5be908627c7c2e6
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2022-06-23 01:03:46 +0100

    Calculator

---
+++ Calculator.js
@@ -0,0 +1,36 @@
+  
+let result;
+const operator = prompt('Enter Operaror(either +, -, * or / ): ');
+
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+switch(operator) {
+    case '+':
+        result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`);
+        break;
+        
+    case '-':
+        result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        break;
+        
+    case '*':
+        result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`);
+        break;
+        
+    case '/':
+        result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        break;
+    default:
+        console.log('Invalid operator');
+        break;
+
+}
+
+
+
+
